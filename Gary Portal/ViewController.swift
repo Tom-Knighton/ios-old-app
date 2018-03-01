add6dd39d0ec4d68bfa6cd63a9cfbe14d66d3527
farmingPotatoes <tomknighton@icloud.com>
2018-03-01 15:07:32 +0000

    Initial Commit

---
+++ Gary Portal/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Gary Portal
+//
+//  Created by Tom Knighton on 01/03/2018.
+//  Copyright © 2018 Tom Knighton. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
